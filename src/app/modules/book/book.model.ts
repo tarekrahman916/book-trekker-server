953620623175f1d715dbb6d2c5fefa9a80cd7ccd
tarekrahman916 <tarekrahman237@gmail.com>
2023-07-14 00:29:26 +0600

    [Done] added book intrface and model

---
+++ src/app/modules/book/book.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const BookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    publicationDate: {
+      type: Date,
+      required: true,
+    },
+    reviews: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Book = model("Book", BookSchema);
+
+export default Book;
